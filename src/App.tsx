ece059ce071d02f288c1863389d5b6d0d42a5253
Micah <contact_micah@yahoo.com>
2024-05-21 10:52:37 -0700

    10:52

--- src/App.tsx
+++ src/App.tsx
@@ -44,8 +44,18 @@ function App() {
   return (
     
  
+  return (
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <button onClick={printUserAttributes}>Print Attributes</button>
+        </main>
+      )}
+    </Authenticator>
+  );
     
-    <Authenticator hideSignUp loginMechanisms={['useremail']}>
+    <Authenticator hideSignUp loginMechanisms={['email']}>
       {({ signOut, user }) => (
 
 
@@ -70,14 +80,7 @@ function App() {
 
           <button onClick={signOut}>Sign out</button>
 
-          <h1>Hello {user.email}   </h1>
-          
-
-      
-    
-          <p>Email: 
-          </p>
-          <p>{user.email}</p>
+          <h1>Hello {user.username}<   </h1>
 
           <button onClick={printUserEmail}>Print Attributes</button>
 
