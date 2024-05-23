6872eccf85e26c6d734ac86e74627b1e0b52a302
Micah <contact_micah@yahoo.com>
2024-05-23 12:39:09 -0700

    git added about page manually

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        This is a simple About page. You can add details about yourself, your interests,
+        or anything you'd like to share with visitors.
+      </p>
+    </div>
+  );
+}
+
+export default About;
+
