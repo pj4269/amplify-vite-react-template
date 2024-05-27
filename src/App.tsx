5ff1345f8406f7374da510b3d6794287002fc5ff
Micah <contact_micah@yahoo.com>
2024-05-27 12:00:55 -0700

    took out Profile-2

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
               </li>
 
               <li>
-                <Link to="/profile">Profile</Link>
+                {/*<Link to="/profile">Profile</Link> */}
               </li>
               <li>
                 <Link to="/about">About</Link>
