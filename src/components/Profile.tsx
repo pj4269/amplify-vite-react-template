6da7f574b9eebd4414d47c9eed48c260598e0b57
Micah <contact_micah@yahoo.com>
2024-05-27 12:23:16 -0700

    re-created Profile.tsx

---
+++ src/components/Profile.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { fetchUserAttributes } from 'aws-amplify/auth';
+
+function ProfilePage() {
+  const [userInfo, setUserInfo] = useState({ email: '' });
+  const [editedEmail, setEditedEmail] = useState('');
+
+  const fetchEmail = async () => {
+    try {
+      const user = await fetchUserAttributes();
+      const email = user.email ?? ''; // Ensure email is not undefined
+      setUserInfo(prevState => ({
+        ...prevState,
+        email: email
+      }));
+      setEditedEmail(email); // Update editedEmail with fetched email
+    } catch (error) {
+      console.log('Error fetching email: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEmail();
+  }, []);
+
+  return (
+    <div className="profile">
+      <h2>Profile Page</h2>
+      <div className="profile-details">
+        <p><strong>Email:</strong> {userInfo.email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
+
