8c0a638f0588f547a9a65768ed0cb3c06644daa0
Micah <contact_micah@yahoo.com>
2024-05-27 13:21:00 -0700

    updateUserAttribute 2  Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchUserAttributes, updateUserAttribute } from 'aws-amplify/auth';
 
 function ProfilePage() {
@@ -26,9 +26,7 @@ function ProfilePage() {
 
   const handleSave = async () => {
     try {
-      //await updateUserAttribute('email', editedEmail);
-      await updateUserAttribute({ email: editedEmail });
-      
+      await updateUserAttribute({ email: editedEmail }, {});
       setIsEditing(false);
       // Update userInfo with the edited email
       setUserInfo(prevState => ({
