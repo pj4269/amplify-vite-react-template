c15b7f0973688a54431ab2c0ac0eaa251e11f18b
Micah <contact_micah@yahoo.com>
2024-06-05 07:10:56 -0700

    Jun 05

--- src/components/SendNumber.tsx
+++ src/components/SendNumber.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react';
 import axios from 'axios'; // Ensure you've installed axios
 
 function SendMyNumber() {
-  const numberToSend = 100;
+  const [numberToSend, SetnumToSend] = useState(100);
   const [result, setResult] = useState(null);
 
   const handleClick = async () => {
     try {
-      // Works with LocalHost
+      // Works with LocalHost  : FastApi address
       //const response = await  axios.post('http://localhost:8000/add', { number: numberToSend }, {headers: { 'Content-Type': 'application/json' }
-      const response = await  axios.post('https://avyh2v3z2o4hueu5nepvhjglke0rhedc.lambda-url.us-west-2.on.aws/add', 
-                                         { number: numberToSend }, {headers: { 'Content-Type': 'application/json' }      
+      //const response = await  axios.post('https://avyh2v3z2o4hueu5nepvhjglke0rhedc.lambda-url.us-west-2.on.aws/add', 
+      //                                   { number: numberToSend }, {headers: { 'Content-Type': 'application/json' }     
+      // Maybe I should use Lambda ARN address instead 
+      const response = await  axios.post('arn:aws:lambda:us-west-2:711141261525:function:June_03_24_Python_3_10/add', { number: numberToSend }, {headers: { 'Content-Type': 'application/json' }
+      
                           });
       setResult(response.data.result);
       }
